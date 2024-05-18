4839f2f26e0c11e9f709cbac395ea6d9923d9256
DawidSzoka1 <oficer130@gmail.com>
2024-05-18 18:42:56 +0200

    80 but to slow

---
+++ interviewQuestions/80_remove_duplicates_2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        for i in nums:
+            while nums.count(i) > 2:
+                nums.remove(i)
+        return len(nums)
+
+
+nums = [0,0,0,0,0]
+Solution().removeDuplicates(nums)
+print(nums)
+
